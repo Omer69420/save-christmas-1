0bd56a0c7637bea7cb70b16a76258c3de142f207
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-23 14:52:31 +0100

    loops

---
+++ loop-warmups.js
@@ -0,0 +1,18 @@
+// while-loop
+
+while (i < 5) {
+    text += "The number is " + i;
+    i++;
+  }
+console.log(i)
+
+
+
+
+
+// for-loop
+
+let i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
