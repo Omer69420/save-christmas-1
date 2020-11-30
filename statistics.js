aa5e758fe79269988436a399595cd96a2af47536
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-30 14:14:33 +0100

    medelvärde

--- statistics.js
+++ statistics.js
@@ -14,3 +14,25 @@ function min() {
 
 min();
 
+const all = [1, 4, 8, 12, 16, 20];
+
+function max() {
+    let arr = Math.max(...all);
+    console.log(arr)
+}
+
+max();
+
+function min() {
+    let arr = Math.min(...all);
+    console.log(arr)
+}
+
+min();
+
+function mean() {
+    let arr = (alla[0] + alla[1] + alla[2] + alla[3] + alla[4] + alla[5]) / 6
+    console.log(Math.round(arr))
+}
+
+mean();
